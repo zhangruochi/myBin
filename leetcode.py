7043a29aa5e307ba4423d2d229af2285cd15ef3d
zhangruochi <zrc720@gmail.com>
2020-02-07 02:29:06 -0500

    2020-02-07

---
+++ leetcode.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import sys
+from pathlib import PosixPath as P
+import os
+
+
+
+problem_name = sys.argv[1]
+cur_work_dir = P("/Users/ZRC/Desktop/leetcode")
+
+
+cur_work_dir = cur_work_dir / problem_name
+
+if not cur_work_dir.exists():
+    (cur_work_dir).mkdir()
+    (cur_work_dir/"solution.py").touch()
+    (cur_work_dir/"solution.java").touch()
+    (cur_work_dir/"solution.c++").touch()
+    (cur_work_dir/"note.md").touch()
+
+os.system("subl {}".format(cur_work_dir))
+
+
+
+
+
+
+
